54176c09be195abc468f4e20f0b865fa2d532643
RaFaElMOW <rafaelmowgfx@gmail.com>
2019-04-09 18:40:10 -0300

    Telas cadastro responsável e criança

---
+++ app/src/main/java/com/example/babycare/ViewCadastroCrianca.java
@@ -0,0 +1,13 @@
+package com.example.babycare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewCadastroCrianca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cadastro_crianca);
+    }
+}
---
+++ app/src/main/java/com/example/babycare/ViewCadastroResponsavel.java
@@ -0,0 +1,35 @@
+package com.example.babycare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ViewCadastroResponsavel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cadastro);
+
+        Button cancelar = (Button) findViewById(R.id.btnCancelar);
+        Button cadastrar = (Button) findViewById(R.id.btnEfetuarCadastro);
+
+        cancelar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+        cadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+                Intent it = new Intent(ViewCadastroResponsavel.this, ViewCadastroCrianca.class);
+                startActivity(it);
+            }
+        });
+    }
+}
